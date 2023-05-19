5dc1792511277d069830f4bbad0657dc35aa0643
monier87 <carlos.monier87@gmail.com>
2023-05-19 16:02:08 -0400

    Add Picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { HiMenu, HiX } from "react-icons/hi";
+import MenuOverlay from "./MenuOverlay";
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
+
   const menuList = [
     {
       id: 1,
@@ -32,38 +36,35 @@ function Header() {
         </h2>
       </div>
       <div className="md:ml-4">
-        <div className="flex md:hidden">
-          <svg
-            className="w-6 h-6 text-gray-600 cursor-pointer"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </div>
         <div className="hidden md:flex gap-4">
           {menuList.map((item) => (
             <div key={item.id}>
-              <h2 className="text-red-500 hover:text-white hover:bg-red-500 rounded-full text-base md:text-lg px-3 py-1 cursor-pointer">
+              <h2 className="text-black hover:text-white hover:bg-red-500 rounded-full text-base md:text-lg px-3 py-1 cursor-pointer">
                 {item.title}
               </h2>
             </div>
           ))}
-          <h2 className="text-red-500 hover:text-white hover:bg-red-500 rounded-full text-base md:text-lg px-3 py-1 cursor-pointer">
+          <h2 className="text-black hover:text-white hover:bg-red-500 rounded-full text-base md:text-lg px-3 py-1 cursor-pointer">
             Contrátame
           </h2>
         </div>
       </div>
+      <div className="md:hidden">
+        {!toggle ? (
+          <HiMenu
+            onClick={() => setToggle(!toggle)}
+            className="text-black text-2xl cursor-pointer"
+          />
+        ) : (
+          <HiX
+            onClick={() => setToggle(!toggle)}
+            className="text-black text-2xl cursor-pointer"
+          />
+        )}
+        {toggle ? <MenuOverlay menuList={menuList} /> : null}
+      </div>
     </div>
   );
 }
-
 
 export default Header;
