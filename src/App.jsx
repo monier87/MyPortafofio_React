24c3c6ab75fe6fd13b37ca4d749ce0bdcb181510
Carlos David <carlos.monier87@gmail.com>
2023-05-28 01:08:28 -0600

    Segundo Commit en CMENU

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="min-h-screen bg-gradient-to-tr from-yellow-200 to-green-200 px-8 md:px-14 lg:px-36 pb-10 pt-7">
