51d70c1244057772b84a18ffb57930a550e81598
monier87 <carlos.monier87@gmail.com>
2023-05-31 16:37:48 -0400

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,20 @@ const Footer = () => {
   return (
     <div className='flex justify-center items-center py-6 bg-blue-200'>
       <div className='flex space-x-4'>
-        <FaTwitter className='cursor-pointer' size={20} />
-        <FaGithub className='cursor-pointer' size={20} />
-        <FaInstagram className='cursor-pointer' size={20} />
-        <FaLinkedinIn className='cursor-pointer' size={20} />
+        <span className="font-semibold">Estoy en: </span>
+        <a href='https://twitter.com/tu_perfil_de_twitter' target='_blank' rel='noopener noreferrer'>
+          <FaTwitter className='cursor-pointer' size={20} />
+        </a>
+        <a href='https://github.com/tu_perfil_de_github' target='_blank' rel='noopener noreferrer'>
+          <FaGithub className='cursor-pointer' size={20} />
+        </a>
+        <a href='https://instagram.com/tu_perfil_de_instagram' target='_blank' rel='noopener noreferrer'>
+          <FaInstagram className='cursor-pointer' size={20} />
+        </a>
+        <a href='https://linkedin.com/in/tu_perfil_de_linkedin' target='_blank' rel='noopener noreferrer'>
+          <FaLinkedinIn className='cursor-pointer' size={20} />
+        </a>
+        <span className="animate-bounce">Sígueme</span>
       </div>
     </div>
   );
